13c259475b4b44ccebb7a4463390dc80ce76548c
Winnard Arthur <winnardarthur7@gmail.com>
2023-06-11 16:10:29 +0000

    feat: protect authenticated routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,33 @@
+import { signOut, getSession } from "next-auth/react";
+import { NextPageContext } from "next";
+import useCurrentUser from "../hooks/useCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
   return (
     <>
       <div className="text-white">Netflix clone</div>
+      <p className='text-white'>Logged in as {user?.name} and email: {user?.email}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Sign Out
+      </button>
     </>
   );
 }
